1a34880912273707b1c8e2fbeb79483b1b2ea0a1
windsgone <fzywind@gmail.com>
2026-03-09 10:55:13 +0800

    Initial Commit

---
+++ Drink Reminder/Drink_ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Drink_ReminderApp.swift
+//  Drink Reminder
+//
+//  Created by Zhongyang Fan on 2026/3/9.
+//
+
+import SwiftUI
+
+@main
+struct Drink_ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
